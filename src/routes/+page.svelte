40421495bc3acf1cb6e27274d4c81b4a2fd5af55
jmetzg11alpine <jmetzg11@gmail.com>
2025-05-03 16:57:58 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1 class="text-3xl font-bold">Donkey</h1>
+
+<div class="flex flex-col gap-4">
+	<div class="bg-red-500">red</div>
+	<div class="bg-blue-500">blue</div>
+	<div class="bg-green-500">green</div>
+</div>
